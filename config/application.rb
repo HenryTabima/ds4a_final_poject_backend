2ddaa795e12236d7b7859d5581d37966c94ade3c
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-12-10 15:22:09 -0500

    [add] rack fix

--- config/application.rb
+++ config/application.rb
@@ -33,10 +33,10 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get]
+        resource '*', headers: :any, methods: [:get]
       end
     end
   end
